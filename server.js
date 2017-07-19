2a1b6fe10a065456a00c3278122f6c8d2f248951
Rory <srsullivan01@gmail.com>
2017-07-19 19:06:22 -0400

    finished GET routes

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app     = express();
+var port    = 3007;
+
+app.get('/', function(req,res) {
+  res.send("Welcome to Pizza Express!");
+});
+
+app.get('/topping/:type', function(req, res, next) {
+  res.send(`${req.params.type} + pizza! Good choice.`);
+});
+
+app.get('/order/:amount/:size', function(req, res, next) {
+     res.send(`Your order of ${req.params.amount} ${req.params.size} pizzas will be ready shortly.`);
+ });
+
+app.listen(port, function(){
+  console.log('LISTENING ON PORT ' + port);
+  console.log('Are you having fun yet?');
+});
